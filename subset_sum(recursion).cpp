06c17bc99aac7dcce920f0f28d26b4462f4c7d02
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2023-10-20 13:57:31 +0530

    Create subset_sum(recursion).cpp

---
+++ subset_sum(recursion).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int dp[100][100];
+bool knapsack(int n, int weight[], int sum)
+{
+    if (sum == 0)
+        return true;
+    if (n == 0)
+        return false;
+    // include
+    if (weight[n - 1] <= sum)
+        return knapsack(n - 1, weight, sum - weight[n - 1]) || knapsack(n - 1, weight, sum);
+    // excluding
+    else
+        return knapsack(n - 1, weight, sum);
+}
+int main()
+{
+    int weight[] = {2, 3, 7, 8, 10};
+    int sum = 11;
+    memset(dp, -1, sizeof(dp));
+    bool ans = knapsack(5, weight, sum);
+    cout << "your answer is " << ans;
+}
