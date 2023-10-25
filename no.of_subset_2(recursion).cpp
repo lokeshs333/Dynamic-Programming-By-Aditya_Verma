a014c484be8ecacb3a49a149976f1f46bc2c3487
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2023-10-25 22:28:50 +0530

    Create no.of_subset_2(recursion).cpp

---
+++ no.of_subset_2(recursion).cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int knapsack(int n, vector<int>& nums, int csum, int sum, int diff) {
+    if (csum + diff == sum) return 1;
+    if (n == 0) return 0;
+    
+      int l = knapsack(n - 1, nums, csum + nums[n - 1], sum, diff) + knapsack(n - 1, nums, csum, sum, diff);
+        int r = knapsack(n - 1, nums, csum, sum, diff);
+
+    return max(l , r);
+}
+
+int main() {
+    vector<int> nums = {1, 1, 2, 3};
+    int n = nums.size();
+    int sum = 0;
+    for (auto it : nums) sum += it;
+    int diff = 3;
+    int answer = knapsack(n, nums, 0, sum, diff);
+    cout << "Your answer is " << answer << endl;
+
+    return 0;
+}
