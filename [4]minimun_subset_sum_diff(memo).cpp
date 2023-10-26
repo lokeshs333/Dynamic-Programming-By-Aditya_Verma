fcc3d87fe98275fad0557db7cae30e3bf52a5e3b
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2023-10-26 23:17:58 +0530

    Update [4]minimun_subset_sum_diff(memo).cpp

--- [4]minimun_subset_sum_diff(memo).cpp
+++ [4]minimun_subset_sum_diff(memo).cpp
@@ -1,29 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 int dp[100][100];
-auto start = std::chrono::high_resolution_clock::now();
+auto start = chrono::high_resolution_clock::now();
 
-int knapsack(int n, vector<int>& nums, int csum, int sum) {
-    
-    if (n == 0) return abs(sum - csum);
-    if(dp[n][sum]!=-1)return dp[n][sum];
-    int l = knapsack(n-1, nums, csum + nums[n-1], sum - nums[n-1]);
-    int r = knapsack(n-1, nums, csum, sum + nums[n-1]); 
-    dp[n][sum] = min(l, r);
-    return dp[n][sum];
+int minSubsetSumDiff(int n, vector<int>& nums, int csum, int sum) {
+    if (n == 0) 
+        return abs(sum - csum);
+        if(dp[n][csum]!=-1)return dp[n][csum];
+    int includeCurrent = minSubsetSumDiff(n - 1, nums, csum + nums[n - 1], sum-nums[n-1]);
+    int excludeCurrent = minSubsetSumDiff(n - 1, nums, csum, sum);
+     dp[n][csum]= min(includeCurrent, excludeCurrent);
+    return dp[n][csum];
 }
 
 int main() {
-    vector<int> nums = {1,3,5,7,9,13,16,188,100,12,11,11,2,1,21,21,21,2,1,12,1,1,2,1,21,21};
+    vector<int> nums = {1, 2, 3, 4 ,100};
     memset(dp,-1,sizeof(dp));
     int sum = accumulate(nums.begin(), nums.end(), 0);
     int n = nums.size();
-    int answer = knapsack(n, nums, 0, sum);
+    int answer = minSubsetSumDiff(n, nums, 0, sum);
     cout << "Your answer is " << answer << endl;
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> duration = end - start;
-    std::cout << "Compilation time: " << duration.count() << " milliseconds" << std::endl;
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> duration = end - start;
+    cout << "Execution time: " << duration.count() << " milliseconds" << endl;
 
     return 0;
 }
